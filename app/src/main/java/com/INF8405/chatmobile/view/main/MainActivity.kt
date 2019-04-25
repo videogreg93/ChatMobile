1ae2445b020b07ec3e7a1a5f6b3f58c9c9ee3b0a
ErwanBouet <erwanbouet@gmail.com>
2019-04-25 17:39:05 -0400

    stats service try

--- app/src/main/java/com/INF8405/chatmobile/view/main/MainActivity.kt
+++ app/src/main/java/com/INF8405/chatmobile/view/main/MainActivity.kt
@@ -11,6 +11,7 @@ import com.INF8405.chatmobile.system.ChatMobileManagers
 import com.INF8405.chatmobile.view.home.HomeFragment
 import com.INF8405.chatmobile.view.login.LoginActivity
 import com.INF8405.chatmobile.view.profile.ProfileFragment
+import com.INF8405.chatmobile.view.stats.StatsFragment
 import com.INF8405.chatmobile.view.utils.ViewUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -30,6 +31,10 @@ class MainActivity : AppCompatActivity(), MainContract.View {
                 val bundle = Bundle()
                 bundle.putString(ProfileFragment.ID_ARG, myId)
                 ViewUtils.displayFragmentWithArgs(this, ProfileFragment(), false, bundle)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_stats -> {
+                ViewUtils.displayFragmentWithoutArgs(this, StatsFragment(), false)
                 return@OnNavigationItemSelectedListener true
             }
         }
